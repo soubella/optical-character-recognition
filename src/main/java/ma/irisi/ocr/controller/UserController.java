d981e56ef349fb1447fb4e2591f047f28a8c411c
moussaidOualid <moussaid.oualid@gmail.com>
2020-01-06 15:19:31 +0100

    user rest api added

--- src/main/java/ma/irisi/ocr/controller/UserController.java
+++ src/main/java/ma/irisi/ocr/controller/UserController.java
@@ -1,6 +1,51 @@
 package ma.irisi.ocr.controller;
-import org.springframework.web.bind.annotation.RestController;
+import ma.irisi.ocr.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 public class UserController {
+    @RequestMapping("test")
+    public String test() {
+        return "Hello dear";
+    }
+
+    @RequestMapping(
+            value = "/users/registder",
+            method = RequestMethod.POST)
+    public void process(@RequestBody User user)
+            throws Exception {
+
+        System.out.println("user");
+
+    }
+
+    private User user;
+    @CrossOrigin(origins = "*")
+    @PostMapping(path= "/users/register", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addUser(@RequestBody User user)
+            throws Exception
+    {
+        //Generate resource id
+
+        user.setId((long) 1);
+        System.out.println(user.getId());
+        //add resource
+        //User.addUser(user);
+
+        //Create resource location
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        //Send location in response
+        return ResponseEntity.created(location).build();
+    }
+
 }
