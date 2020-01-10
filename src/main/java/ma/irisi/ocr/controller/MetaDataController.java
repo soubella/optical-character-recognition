3cc6691cb4681a42e419bc6d09a9e31b025b90b0
samoouGit <moha.ousaid.saaid@gmail.com>
2020-01-10 12:08:51 +0100

    implement MetaDataController

--- src/main/java/ma/irisi/ocr/controller/MetaDataController.java
+++ src/main/java/ma/irisi/ocr/controller/MetaDataController.java
@@ -1,6 +1,34 @@
 package ma.irisi.ocr.controller;
+import ma.irisi.ocr.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MetaDataController {
+
+    @Autowired
+    private FileController fileService;
+
+    @Autowired
+    private UserController userService;
+
+    @Autowired
+    private User user;
+
+    @PostMapping(value = "/addNote")
+    public ModelAndView createNote(@RequestParam(value = "subject") String subject, @RequestParam(value = "text") String text, @RequestParam(value = "id") String id) {
+        ModelAndView modelAndView = new ModelAndView("file");
+        modelAndView.addObject("file", fileService);
+        modelAndView.addObject("metadata", fileService);
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/showAllMetadata")
+    public String getAllNotes() {
+        return userService.userRepository.toString();
+    }
 }
